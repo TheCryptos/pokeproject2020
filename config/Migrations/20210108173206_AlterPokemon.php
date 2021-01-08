f5e9532fbd3c54c5879f5a30412f34a9e0f1b6b4
Emedo80 <emeric.dorion@gmailcom>
2021-01-08 19:21:25 +0100

    [+] Ajout default_back_sprite_url et front_shiny_sprite_url nullable

---
+++ config/Migrations/20210108173206_AlterPokemon.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AlterPokemon extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('pokemons');
+        $table->addColumn('default_back_sprite_url', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('front_shiny_sprite_url', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->update();
+    }
+}
