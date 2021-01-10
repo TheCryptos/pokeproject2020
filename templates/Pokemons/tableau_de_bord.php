43f008ab8f1a3f387f07aeb601902ef6db512264
TheCryptos <C.ELenoir@outlook.fr>
2021-01-10 08:00:28 +0100

    [*] Définition d'une variable par défaut pour la fonction tableauDeBord templates/Pokemons/tableau_de_bord.php

--- templates/Pokemons/tableau_de_bord.php
+++ templates/Pokemons/tableau_de_bord.php
@@ -1,4 +1,4 @@
-<div class="">
+<div class="content">
   <ul>
     <h3>Poids moyen des pokemons de la 4ème génération</h3>
     <p>Résultat = <?= h($query1->first()->PoidsMoyen) ?></p>
@@ -6,16 +6,14 @@
     <h3>Nombre de pokemons de type fée entre les générations 1,3 et 7</h3>
     <p>Résultat = <?= h($query2->first()->NombresPokemonsFée) ?></p>
 
+
     <h3>Les 10 premiers pokemons qui possède la plus grande vitesse</h3>
     <ul>
-      <?php
-        $i=0;
-        foreach ($query3 as $pokemons) { ?>
-          <li><?php echo $pokemons;  ?></li>
-          <?php
-          $i++;
-        }
-      ?>
+      <?php foreach ($query3 as $key => $pokemons) : ?>
+          <li><?php echo $key.$pokemons;  ?></li>
+          <?php endforeach; ?>
+
+      </ul>
     </ul>
-  </ul>
+  </div>
 </div>
