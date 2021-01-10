f0474df5113259f31800c5f5e33a2198a2d68ae8
Zinkoooo <aymeric.lecas@orange.fr>
2021-01-10 02:03:02 +0100

    [*] Ajout function tableauDeBord /src/Controller/PokemonsController.php

--- src/Controller/PokemonsController.php
+++ src/Controller/PokemonsController.php
@@ -29,6 +29,40 @@ class PokemonsController extends AppController
     }
 
     /**
+     * tableauDeBord method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function tableauDeBord($type)
+    {
+      $query1 = $this->Pokemons->find();
+      $query1->where(['id > 386 AND id < 494'])->select(['PoidsMoyen' => $query1->func()->avg('weight')]);
+
+      /* Première façon pas très opti car il y a un traitement à faire dans la tamplate tableau_de_bord avec 2 boucles foreach et un if
+      $query2 = $this->Pokemons->find()
+             ->where(['id > 1 AND id < 152 OR id > 251 AND id < 387 OR id > 721 AND id < 810']) //Pokémons des générations 1, 3 et 7
+             ->contain(['PokemonTypes.Types']) //Jointure entre 2 tables
+             ->extract('pokemon_types');
+      */
+
+      $query2 = $this->Pokemons->PokemonTypes->find()
+        ->contain(['Types'])
+        ->where(['Types.name' => "$type", '(pokemon_id > 0 AND pokemon_id < 152 OR pokemon_id > 251 AND pokemon_id < 387 OR pokemon_id > 721 AND pokemon_id < 810)']);
+
+      $query2->select(['NombresPokemonsFée' => $query2->func()->count('name')]); //On compte le nombre de pokemons de type fée
+
+      $query3 = $this->Pokemons->PokemonStats->find()
+        ->contain(['Stats'])
+        ->where(['Stats.name' => "speed"])
+        ->order(['value' => 'DESC']);
+
+      $query3->limit(10)->count();
+      $query3->select(['pokemon_id', 'value']);
+
+      $this->set(compact('query1', 'query2', 'query3'));
+    }
+
+    /**
      * View method
      *
      * @param string|null $id Pokemon id.
