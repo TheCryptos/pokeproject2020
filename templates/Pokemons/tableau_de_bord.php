b4fe0946da106221f6d62247ef5e74ba15abd6ca
Zinkoooo <aymeric.lecas@orange.fr>
2021-01-10 22:41:06 +0100

    [*] Ajout de l'affichage résultat du nombre de pokémon

--- templates/Pokemons/tableau_de_bord.php
+++ templates/Pokemons/tableau_de_bord.php
@@ -3,10 +3,12 @@
     <h3>Poids moyen des pokemons de la 4ème génération</h3>
     <p>Résultat = <?= h($query1->first()->PoidsMoyen) ?></p>
 
-    <h3>Nombre de pokemons de type fée entre les générations 1,3 et 7</h3>
-      <?php foreach ($query2 as $pokemon) : ?>
-                  <?= $this->element('Pokemons/card', ['pokemon' => $pokemon]); ?>
-      <?php endforeach; ?>
+      <h3>Nombre de pokemons de type fée entre les générations 1,3 et 7</h3>
+      <p>Résultat = <?= h($query2->count()) ?> pokémons</p>
+        <h4>Voici leurs cartes :</h4>
+        <?php foreach ($query2 as $pokemon) : ?>
+                    <?= $this->element('Pokemons/card', ['pokemon' => $pokemon]); ?>
+        <?php endforeach; ?>
 
 
     <h3>Les 10 premiers pokemons qui possède la plus grande vitesse</h3>
