c5fbfdb274f5583bbd6c6d4701ba7fe4782de9a4
Zinkoooo <aymeric.lecas@orange.fr>
2021-01-10 22:45:33 +0100

    [*] Correction balise html /templates/Pokemons/tableau_de_bord_.php

--- templates/Pokemons/tableau_de_bord.php
+++ templates/Pokemons/tableau_de_bord.php
@@ -5,19 +5,17 @@
 
       <h3>Nombre de pokemons de type fée entre les générations 1,3 et 7</h3>
       <p>Résultat = <?= h($query2->count()) ?> pokémons</p>
-        <h4>Voici leurs cartes :</h4>
-        <?php foreach ($query2 as $pokemon) : ?>
-                    <?= $this->element('Pokemons/card', ['pokemon' => $pokemon]); ?>
-        <?php endforeach; ?>
+      <h4>Voici leurs cartes :</h4>
+      <?php foreach ($query2 as $pokemon) : ?>
+        <?= $this->element('Pokemons/card', ['pokemon' => $pokemon]); ?>
+      <?php endforeach; ?>
 
 
     <h3>Les 10 premiers pokemons qui possède la plus grande vitesse</h3>
     <ul>
-          <?php foreach ($query3 as $pokemon) : ?>
-                      <?= $this->element('Pokemons/card', ['pokemon' => $pokemon]); ?>
-          <?php endforeach; ?>
-
-      </ul>
+    <?php foreach ($query3 as $pokemon) : ?>
+      <?= $this->element('Pokemons/card', ['pokemon' => $pokemon]); ?>
+    <?php endforeach; ?>
     </ul>
-  </div>
+  </ul>
 </div>
