814a4631e0541de20b8131b2fa0d57c6b22c7da3
Emedo80 <emeric.dorion@gmail.com>
2021-01-10 22:24:29 +0100

    [*]Mise en place des Cards pour les pokemons type Fairy et Rapide

--- templates/Pokemons/tableau_de_bord.php
+++ templates/Pokemons/tableau_de_bord.php
@@ -4,13 +4,15 @@
     <p>Résultat = <?= h($query1->first()->PoidsMoyen) ?></p>
 
     <h3>Nombre de pokemons de type fée entre les générations 1,3 et 7</h3>
-    <p>Résultat = <?= h($query2->first()->NombresPokemonsFée) ?></p>
+      <?php foreach ($query2 as $pokemon) : ?>
+                  <?= $this->element('Pokemons/card', ['pokemon' => $pokemon]); ?>
+      <?php endforeach; ?>
 
 
     <h3>Les 10 premiers pokemons qui possède la plus grande vitesse</h3>
     <ul>
-      <?php foreach ($query3 as $key => $pokemons) : ?>
-          <li><?php echo $key.$pokemons;  ?></li>
+          <?php foreach ($query3 as $pokemon) : ?>
+                      <?= $this->element('Pokemons/card', ['pokemon' => $pokemon]); ?>
           <?php endforeach; ?>
 
       </ul>
