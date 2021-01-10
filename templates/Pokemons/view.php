8983699480801313fe67d8edeb926a0b675c5303
TheCryptos <C.ELenoir@outlook.fr>
2021-01-10 06:29:40 +0100

    [*] Gestion complète des vues des pokémon templates/Pokemons/view.php

--- templates/Pokemons/view.php
+++ templates/Pokemons/view.php
@@ -4,51 +4,104 @@
  * @var \App\Model\Entity\Pokemon $pokemon
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(__('Delete Pokemon'), ['action' => 'delete', $pokemon->id], ['confirm' => __('Are you sure you want to delete # {0}?', $pokemon->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Pokemons'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
+<div class="content">
     <div class="column-responsive column-80">
-      <div class="pokemons view content">
-        <h3><?= h($pokemon->name) ?></h3>
-        <div class="slider">
-          <img src="<?= h($pokemon->default_front_sprite_url) ?>" alt="default_front_sprite">
-          <img src="<?= h($pokemon->default_back_sprite_url) ?>" alt="default_back_sprite_url">
-          <img src="<?= h($pokemon->front_shiny_sprite_url) ?>" alt="front_shiny_sprite_url">
-        </div>
+            <h3 class="high_label"><?= h($pokemon->name) ?></h3>
 
-        <h3><?= $pokemon->first_type ?></h3>
-        <?php if ($pokemon->has_second_type) : ?>
-        <h3><?= $pokemon->second_type ?></h3>
-        <?php endif ?>
-        <table>
+            <table class ="spec">
+              <th><?= $this->Form->postLink(__('Supprimer le pokémon'), ['action' => 'delete', $pokemon->id], ['confirm' => __('Etes-vous sûr que vous voulez supprimer # {0} ?', $pokemon->id), 'class' => 'warning']) ?></th>
+              <th><?= $this->Html->link(__('Liste des pokémon'), ['action' => 'index'], ['class' => 'redirect']) ?></th>
+            </table>
+
+
+<?php if(!empty(h($pokemon->default_front_sprite_url)) OR !empty(h($pokemon->default_back_sprite_url)) OR !empty(h($pokemon->front_shiny_sprite_url))) : ?>
+
+<div id="carousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <?php if(!empty(h($pokemon->default_front_sprite_url))) : ?>
+    <li data-target="#carousel" data-slide-to="0"></li>
+    <?php endif; ?>
+    <?php if(!empty(h($pokemon->default_back_sprite_url))) : ?>
+    <li data-target="#carousel" data-slide-to="1"></li>
+    <?php endif; ?>
+    <?php if(!empty(h($pokemon->front_shiny_sprite_url))) : ?>
+    <li data-target="#carousel" data-slide-to="2"></li>
+    <?php endif; ?>
+  </ol>
+  <div class="carousel-inner">
+        <?php if(!empty(h($pokemon->default_front_sprite_url))) : ?>
+    <div class="carousel-item active">
+      <img class="d-block carousel" src="<?= h($pokemon->default_front_sprite_url) ?>" alt="Front Sprite">
+      <div class="d-none d-md-block">
+        <h1 class="label">Front Sprite</h1>
+      </div>
+    </div>
+        <?php endif; ?>
+        <?php if(!empty(h($pokemon->default_back_sprite_url))) : ?>
+    <div class="carousel-item">
+      <img class="d-block carousel" src="<?= h($pokemon->default_back_sprite_url) ?>" alt="Back Sprite">
+      <div class="d-none d-md-block">
+        <h1 class="label">Back Sprite</h1>
+      </div>
+    </div>
+        <?php endif; ?>
+        <?php if(!empty(h($pokemon->front_shiny_sprite_url))) : ?>
+    <div class="carousel-item">
+      <img class="d-block carousel" src="<?= h($pokemon->front_shiny_sprite_url) ?>" alt="Front Shiny">
+      <div class="d-none d-md-block">
+        <h1 class="label">Front Shiny</h1>
+      </div>
+    </div>
+        <?php endif; ?>
+  </div>
+  <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Précédant</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Suivant</span>
+  </a>
+</div>
+<?php else : ?>
+  <img src="../../webroot/img/pokedex.Webp" alt="Pokémon image not found" style="display: grid;">
+  <h3>Pokédex not found image &#128546;</h3>
+<?php endif; ?>
+
+
+      <table>
+        <td><h3 class ="types"><?= $pokemon->first_type ?></h3></td>
+        <td>
+          <?php if ($pokemon->has_second_type) : ?>
+          <h3 class ="types"><?= $pokemon->second_type ?></h3>
+          <?php endif ?>
+        </td>
+      </table>
+
+        <table class="spec">
           <tr>
-            <th> <h3>HP</h3> </th>
-            <th><h1><?= h($pokemon->Hp) ?></h1></th>
+            <th class="spec"> <h3>HP</h3> </th>
+            <th class="spec"><h1><?= h($pokemon->Hp) ?></h1></th>
           </tr>
           <tr>
-            <th> <h3>Defense</h3> </th>
-            <th><h1><?= h($pokemon->Defense) ?></h1></th>
+            <th class="spec"> <h3>Defense</h3> </th>
+            <th class="spec"><h1><?= h($pokemon->Defense) ?></h1></th>
           </tr>
           <tr>
-            <th> <h3>Attack</h3> </th>
-            <th><h1><?= h($pokemon->Attack) ?></h1></th>
+            <th class="spec"> <h3>Attack</h3> </th>
+            <th class="spec"><h1><?= h($pokemon->Attack) ?></h1></th>
           </tr>
           <tr>
-            <th> <h3>Special Attack</h3> </th>
-            <th><h1><?= h($pokemon->SpeAttack) ?></h1></th>
+            <th class="spec"> <h3>Special Attack</h3> </th>
+            <th class="spec"><h1><?= h($pokemon->SpeAttack) ?></h1></th>
           </tr>
           <tr>
-            <th> <h3>Special Defense</h3> </th>
-            <th><h1><?= h($pokemon->SpeDefense) ?></h1></th>
+            <th class="spec"> <h3>Special Defense</h3> </th>
+            <th class="spec"><h1><?= h($pokemon->SpeDefense) ?></h1></th>
           </tr>
           <tr>
-            <th> <h3>Speed</h3> </th>
-            <th><h1><?= h($pokemon->Speed) ?></h1></th>
+            <th class="spec"> <h3>Speed</h3> </th>
+            <th class="spec"><h1><?= h($pokemon->Speed) ?></h1></th>
           </tr>
         </table>
       </div>
